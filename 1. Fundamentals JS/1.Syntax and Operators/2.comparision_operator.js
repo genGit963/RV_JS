8749a0f139027d082b605c94114c4d6621073505
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-29 13:09:07 +0545

    2.comparision_operator.js: learning

---
+++ 1. Fundamentals JS/1.Syntax and Operators/2.comparision_operator.js
@@ -0,0 +1,15 @@
+/*
+== (Loose Equality): 
+This operator checks for equality after converting the 
+operands to the same type (type coercion).
+*/
+console.log("5 == '5' : ", 5 == "5"); // true, because '5' (string) is converted to 5 (number)
+
+/*
+=== (Strict Equality): 
+This operator checks for equality without
+performing type conversion. 
+
+The values must be of the same type as well as the same value.
+*/
+console.log("5 === '5' : ", 5 === "5"); // false, because the types are different (number and string)
