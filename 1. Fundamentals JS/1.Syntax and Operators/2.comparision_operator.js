ccb84a0cce064966d1e0830159fa0add077b109a
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-29 13:10:24 +0545

    2.comparision_operator.js: learning

--- 1. Fundamentals JS/1.Syntax and Operators/2.comparision_operator.js
+++ 1. Fundamentals JS/1.Syntax and Operators/2.comparision_operator.js
@@ -3,7 +3,7 @@
 This operator checks for equality after converting the 
 operands to the same type (type coercion).
 */
-console.log("5 == '5' : ", 5 == "5", "5 != '5' : ", 5 != "5"); // true, because '5' (string) is converted to 5 (number)
+console.log("5 == '5' : ", 5 == "5", "\n5 != '5' : ", 5 != "5"); // true, because '5' (string) is converted to 5 (number)
 
 /*
 === or !== (Strict Equality): 
@@ -12,4 +12,4 @@ performing type conversion.
 
 The values must be of the same type as well as the same value.
 */
-console.log("5 === '5' : ", 5 === "5", "5 !== '5' : ", 5 !== "5"); // false, because the types are different (number and string)
+console.log("5 === '5' : ", 5 === "5", "\n5 !== '5' : ", 5 !== "5"); // false, because the types are different (number and string)
