1730fe83a4f4cc9f5834b0fc3a6af638d2d2c547
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-29 16:53:37 +0545

    2_reference_type.js: learning

--- 1. Fundamentals JS/2.Data Types/2_reference_type.js
+++ 1. Fundamentals JS/2.Data Types/2_reference_type.js
@@ -17,6 +17,7 @@ reference types:
 /*
 Definition: 
     Objects are collections of key-value pairs. 
+
     Each key (or property) in an object points to 
     a value, which could be anything 
     (a string, number, function, or even another object).
@@ -39,3 +40,35 @@ console.log(person.name); // "John"
 console.log(person["age"]); // 30
 person.name = "heppy";
 console.log(person.name); // "John"
+
+// Arrays
+/*
+Definition: 
+    Arrays are ordered collections of items (like a list).
+    Each item in the array can be of any type, 
+    and arrays are great for storing multiple related items together.
+
+How they work: 
+    Each item in an array has an index (starting from 0), 
+    which allows you to access or modify the value at that position.
+*/
+let colors = ["red", "blue", "green"];
+// Accessing values: You can access items in an array using their index
+console.log(colors[0]); // "red"
+console.log(colors[2]); // "green"
+
+/*
+Why They're Called "Reference Types"?
+
+When you assign an object or an array to a variable, 
+you’re actually assigning a reference to that object 
+or array, not the actual data. 
+
+This means if you copy an object or array 
+into another variable, 
+both variables will point to the same data.
+*/
+let a = [1, 2, 3];
+let b = a; // b now refers to the same array as a
+b.push(4); // Adding a value to array b
+console.log(a); // [1, 2, 3, 4] - Both a and b are affected!
