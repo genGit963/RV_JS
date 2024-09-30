509024a5aa4564e254932ba1da4a8075905ff18b
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-30 14:04:32 +0545

    Control Structures

--- 1. Fundamentals JS/3.Control Structures/Control_Structures.js
+++ 1. Fundamentals JS/3.Control Structures/Control_Structures.js
@@ -13,9 +13,36 @@ Purpose:
 
 Use Case: 
     Best used for objects.
+
+Syntax: 
+    for (let i in object){
+        // code to be executed
+    }
 */
 const person = { name: "John", age: 30, city: "New York" };
 for (let key in person) {
   console.log(key); // name, age, city
   console.log(person[key]); // John, 30, New York
+}
+
+// for...of
+/*
+Purpose: 
+    Iterates over the values of iterable objects 
+    like arrays, strings, maps, sets, etc.
+
+Use Case: 
+    Best used for iterables 
+    (e.g., arrays, strings, sets, maps).
+
+Syntax:
+    for (let value of iterable) {
+        // code block to be executed
+    }
+
+*/
+
+const array = ["apple", "banana", "cherry"];
+for (let fruit of array) {
+  console.log(fruit); // apple, banana, cherry
 }
