9c328e9b6858237614164984dd4f1f133cda36c3
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-09-30 14:14:02 +0545

    1_declaration_VS_expressions.js

---
+++ 1. Fundamentals JS/4.Functions/1_declaration_VS_expressions.js
@@ -0,0 +1,51 @@
+//       - Function declarations vs. expressions
+/*
+    "function declarations" and "function expressions" 
+    are TWO WAYS to define functions.
+
+    They differ in syntax, behavior, and how they are hoisted.
+ */
+
+// 1. Function Declarations
+/**
+ * Naming: 
+    Function declarations must have a name.
+
+ * Syntax:
+    function functionName(parameters) {
+    // function body
+    }
+
+ * Hoisting: 
+    Function declarations are hoisted, 
+    meaning you can call the function before its declaration in the code. 
+    JavaScript moves the function declaration 
+    to the top of the scope during the compile phase.
+ */
+sayHello(); // Works due to hoisting
+
+function sayHello() {
+  console.log("Hello!");
+}
+
+// 1. Function Declarations
+/**
+ * Naming: 
+    Function declarations must have a name.
+
+ * Syntax:
+    function functionName(parameters) {
+    // function body
+    }
+
+ * Hoisting: 
+    Function declarations are hoisted, 
+    meaning you can call the function before its declaration in the code. 
+    JavaScript moves the function declaration 
+    to the top of the scope during the compile phase.
+ */
+sayHello(); // Works due to hoisting
+
+function sayHello() {
+  console.log("Hello!");
+}
---
+++ 1. Fundamentals JS/4.Functions/Functions.js
@@ -0,0 +1,9 @@
+/*
+ - Functions
+      - Function declarations vs. expressions
+      - Arrow functions and their lexical `this` binding
+      - Higher-order functions: functions that take other functions as arguments or return them
+      - Callbacks and their role in asynchronous programming
+      - Closures: functions that capture the scope in which they were created
+      
+ */
