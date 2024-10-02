fcf836c9849af96c671cbec4c8dff630589c2a1b
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-02 14:55:59 +0545

    4.Functions done

--- 1. Fundamentals JS/4.Functions/Functions.js
+++ 1. Fundamentals JS/4.Functions/Functions.js
@@ -5,5 +5,7 @@
       - Higher-order functions: functions that take other functions as arguments or return them
       - Callbacks and their role in asynchronous programming
       - Closures: functions that capture the scope in which they were created
+      - Immediately invoked function expression (IIFE)
+
       
  */
