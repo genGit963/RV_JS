30e3d03ba00df1dd08ab4d1a6c2cad54d12b6288
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-07 19:40:20 +0545

    1_template_string.js

---
+++ 2. Advanced JS/3.ES6+ Features/1_template_string.js
@@ -0,0 +1,23 @@
+//       - Template literals for multi-line strings and variable interpolation
+/**
+ * Template literals 
+    in JavaScript provide a powerful way to create multi-line strings 
+    and perform variable interpolation. 
+    
+    They are enclosed by backticks (`) instead of single (') 
+    or double quotes (")
+
+    Features of Template literals string:
+        1. Multi-line Strings
+        2. Variable Interpolation
+        3. Using Expressions
+        4. Nesting Template Literals
+        5. Tagged Templates
+*/
+
+// 1. Multi-line Strings
+const multiLineString = `This is a string
+that spans multiple
+lines.`;
+
+console.log(multiLineString);
