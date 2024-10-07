4bcba665670304f4fce4364d932541c018bb3ec5
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-07 20:37:44 +0545

    3_spread_and_rest_parameters.js

---
+++ 2. Advanced JS/3.ES6+ Features/3_spread_and_rest_parameters.js
@@ -0,0 +1,16 @@
+//        - Spread (`...`) and rest parameters (`...args`)
+
+/**
+ * spread operator (...) and the rest parameters (...args) 
+        are two powerful features that enhance how you handle arrays 
+        and function parameters. While they look similar, 
+        they serve different purposes.
+*/
+
+/**
+ * Spread Operator (...)
+        The spread operator allows you to expand elements of an 
+        iterable (like an array or a string) 
+        into individual elements. 
+        It's useful for merging or cloning arrays and objects.
+*/
