5dfb6ac2a2d84a8a0a9fbf7b5745831c10364f9e
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-07 20:44:22 +0545

    3_spread_and_rest_parameters.js

--- 2. Advanced JS/3.ES6+ Features/3_spread_and_rest_parameters.js
+++ 2. Advanced JS/3.ES6+ Features/3_spread_and_rest_parameters.js
@@ -7,6 +7,7 @@
         they serve different purposes.
 */
 
+// 1. Spread Operator (...)
 /**
  * Spread Operator (...)
         The spread operator allows you to expand elements of an 
@@ -14,11 +15,45 @@
         into individual elements. 
         It's useful for merging or cloning arrays and objects.
 */
+// 1. Expanding Arrays
 const numbers = [1, 2, 3];
 const moreNumbers = [4, 5, ...numbers, 6];
 console.log(moreNumbers); // Output: [4, 5, 1, 2, 3, 6]
 
-// cloning array
+// 2. cloning array
 const originalArray = [1, 2, 3];
 const clonedArray = [...originalArray];
 console.log(clonedArray); // Output: [1, 2, 3]
+
+// 3. Merging Arrays
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const mergedArray = [...array1, ...array2];
+console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+// 4. Spreading Objects
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+// Merging objects
+const mergedObj = { ...obj1, ...obj2 };
+console.log(mergedObj); // Output: { a: 1, b: 3, c: 4 }
+
+// 2. Rest Parameters (...args)
+/**
+ *Rest parameters
+        allow you to represent an indefinite number of arguments 
+        as an array. 
+        This is useful when you want to create functions 
+        that can accept varying numbers of parameters.
+
+        Eg: console.log(obj1, obj2, .....objn);
+ */
+// 1: Basic Usage
+function sum(...numbers) {
+  console.log("given parameterss: ", numbers);
+  return numbers.reduce((acc, num) => acc + num, 0);
+}
+console.log(sum(1, 2, 3)); // Output: 6
+console.log(sum(4, 5, 6, 7, 8)); // Output: 30
+
+// 2. Mixing with Regular Parameters
